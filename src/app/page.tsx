7af8355d647342076e0867881344189183c8ba77
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-03-12 02:09:44 -0400

    added form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      
     </div>
   );
 }
